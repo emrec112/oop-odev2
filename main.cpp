53298d34cb7575059bff262237647bdb142ef59c
Emre Camuzoğlu <emrecamuz06@gmail.com>
2024-12-09 21:58:16 +0300

    class tanımları yapıldı

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class personType{
+    private:
+        string firstName;
+        string lastName;
+
+    public:
+        void setStudentName();
+        void print();
+};
+
+class studentType : public personType{
+    private:
+        int numberOfCourses;
+        courseType *course;//gradeyi burdan çekicez
+        int studentId;
+        int isTuitionPaid;
+    
+    public:
+        void print();
+        void print(int one);
+        void print(int one, int two);
+        void setName();
+        void getName();
+        void getHoursEnrolled();
+        void getGpa();
+        void billingAmount();
+        studentType();
+};
+
+class courseType{
+    private:
+        string courseName;
+        int courseNo;
+        char courseGrade;//student sınıfı bunu kullanıcak özellikle
+        char courseCredits;
+
+    public:
+        void setCourseInfo();
+        void print(int one);
+        void print(int one, int two);
+        void getCredits();
+        void getCourseNumber();
+        void getGrade();
+        courseType();
+
+};
+
+int main(){
+    return 0;
+}
