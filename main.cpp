29763af5263cb86df2b6899e82808fddf18ea63e
Emre Camuzoğlu <emrecamuz06@gmail.com>
2024-12-10 18:41:00 +0300

    bazı boktan fonksiyonlar eklendi

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class personType{
@@ -8,28 +9,18 @@ class personType{
         string lastName;
 
     public:
-        void setStudentName();
-        void print();
+        void setStudentName(string firstName, string lastName);
+        void getStudentName();
 };
 
-class studentType : public personType{
-    private:
-        int numberOfCourses;
-        courseType *course;//gradeyi burdan çekicez
-        int studentId;
-        int isTuitionPaid;
-    
-    public:
-        void print();
-        void print(int one);
-        void print(int one, int two);
-        void setName();
-        void getName();
-        void getHoursEnrolled();
-        void getGpa();
-        void billingAmount();
-        studentType();
-};
+void personType::setStudentName(string firstName, string lastName){
+    this->firstName = firstName;
+    this->lastName = lastName;
+}
+
+void personType::getStudentName(){
+    cout << firstName << "||" << lastName << "||";
+}
 
 class courseType{
     private:
@@ -39,18 +30,74 @@ class courseType{
         char courseCredits;
 
     public:
-        void setCourseInfo();
-        void print(int one);
-        void print(int one, int two);
-        void getCredits();
-        void getCourseNumber();
-        void getGrade();
-        courseType();
+        // void setCourseInfo();
+        // void print(int one);
+        // void print(int one, int two);
+        // void getCredits();
+        // void getCourseNumber();
+        // void getGrade();
+        // courseType();
 
 };
 
+class studentType : public personType{
+    private:
+        int numberOfCourses;
+        courseType *course;//gradeyi burdan çekicez
+        string studentId;
+        char isTuitionPaid;
+    
+    public:
+        void print();
+        // void print(int one);
+        // void print(int one, int two);
+        void setInfo(string firstName, string lastName, string id, char isTuitionPaid, int numberOfCourses);
+        // void getHoursEnrolled();
+        // void getGpa();
+        // void billingAmount();
+        studentType();
+};
+
+studentType::studentType(){
+    numberOfCourses = 0;
+    course = NULL;
+    isTuitionPaid = 'N';
+}
+
+void studentType::setInfo(string firstName, string lastName, string id, char isTuitionPaid, int numberOfCourses){
+    setStudentName(firstName, lastName);
+    studentId = id;
+    this->isTuitionPaid = isTuitionPaid;
+    this->numberOfCourses = numberOfCourses;
+}
+
+void studentType::print(){
+    getStudentName();
+    cout << studentId << "||" << isTuitionPaid << "||" << numberOfCourses << endl;
+}
+
 int main(){
-    int number = 5;
-    studentType students[number];
+    int number, tuitionPerHours;
+
+    fstream file("input.txt", ios::in | ios::out | ios::app);
+    file >> number;
+
+    studentType *students = new studentType[number];
+
+    file >> tuitionPerHours;
+
+    string fname, lname, id, line;
+    int nOfcourses;
+    char control;
+    for(int i = 0; i < number; i++){
+        file >> fname >> lname >> id >> control >> nOfcourses;
+        students[i].setInfo(fname, lname, id, control, nOfcourses);
+        while(getline(file, line)){
+            
+        }
+    }
+
+    students[0].print();
+
     return 0;
 }
